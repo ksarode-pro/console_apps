ad89254b5711e885cb3bda62b3ec0a466fb1ac61
Kiran Sarode <ksarode.pro@gmail.com>
2025-01-23 17:29:13 +0530

    comment added to test git commit --amend
    initial commit

---
+++ Pattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//This is a comment added to check git commit --ammend command.
+namespace Pattern
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            for(int i = 1; i <= 5; i++)
+            {
+                for(int j = 1; j <= i; j++ )
+                {
+                    System.Console.Write(j);
+                }
+            System.Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ReverseArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 11, 12, 13, 14, 15, 16 };
+            PrintArray(arr);
+            int[] rev_arr = ReverseArray(arr);
+            PrintArray(rev_arr);
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            Console.WriteLine("Reversed Array elements: ");
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static int[] ReverseArray(int[] arr)
+        {
+            Console.WriteLine("Array elements: ");
+            int temp;
+            for (int x = 0; x < Convert.ToInt32(arr.Length / 2); x++)
+            {
+                //select last element
+                temp = arr[arr.Length - (x + 1)];
+
+                //check if mid point should not be crossed
+                if (x < (arr.Length - (x + 1)))
+                {
+                    arr[arr.Length - (x + 1)] = arr[x];
+                    arr[x] = temp;
+                }
+            }
+            return arr;
+
+        }
+    }
+}
